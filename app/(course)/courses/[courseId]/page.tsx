b325e6d52028f1a0bdc27c73002f434dad349698
HuyNguyen <qhnguyen081100@gmail.com>
2024-07-29 22:29:42 +0700

    create video player page and purchase component

--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -1,7 +1,32 @@
-const CourseIdpage = () => {
-  return (
-    <div>CourseIdpage</div>
-  )
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CourseIdpage = async ({
+  params
+}: {
+  params : { courseId: string; }
+}) => {
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        orderBy: {
+          position: "asc"
+        }
+      }
+    }
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  return redirect(`/courses/${course.id}/chapters/${course.chapters[0].id}`);
 }
 
 export default CourseIdpage
